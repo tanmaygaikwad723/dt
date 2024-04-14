a3f872a97aa99c5ccc6ee6bd9d0b156699d3fdb2
tanmaygaikwad723 <gaikwadtanmay723@gmail.com>
2024-04-14 12:35:33 +0530

    "Template file and models added"

--- gym_management/models.py
+++ gym_management/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Trainer(models.Model):
+    choice = {
+        "m": "male",
+        "f": "female",
+        "o": "other"
+    }
+    name = models.CharField(max_length=30),
+    address = models.CharField(max_length=100),
+    city = models.CharField(max_length=30),
+    gender = models.CharField(max_length=1, choices=choice, default="m"),
+    date_of_birth = models.DateField(),
+    email = models.EmailField(),
+    jod = models.DateField(),
+    trainspec = models.CharField(max_length=30),
+    experience = models.DecimalField(),
+    phone = models.CharField(max_length=13)
+
+    def __str__(self):
+        return f"{self.name},\t{self.gender}\t{self.DOB}\t{self.phone}\t{self.trainspec}"
+        
--- gym_management/views.py
+++ gym_management/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Trainer
 # Create your views here.
+
+# def trainer_details(generic.ListView):
+#     # model = Trainer
+#     # context_object_name = 'Trainer_list'
+#     # template_name = 'gym_management/Trainer_list.html'
+
+def list_trainers(request):
+    trainers = Trainer.objects.all()  # Get all trainer objects
+    context = {'trainers': trainers}
+    return render(request, 'Trainer_list.html', context)
