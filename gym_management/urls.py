627e2a6acbce5c8e09114b9ced8a8dc3c888df35
tanmaygaikwad723 <gaikwadtanmay723@gmail.com>
2024-04-29 18:06:17 +0530

    "edit member_registration template"

--- gym_management/urls.py
+++ gym_management/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     # path('trainers/', views.list_trainers, name="trainers")
+    path("register/member/", views.register_member, name="register_member")
 ]
--- gym_management/views.py
+++ gym_management/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 def home(request):
     template = loader.get_template("gym_management/index.html")
     return HttpResponse(template.render())
+
+from django.shortcuts import render, redirect
+from .forms import MemberRegistrationForm
+
+def register_member(request):
+    if request.method == 'POST':
+        form = MemberRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new member object
+            return redirect('success_url')  # Redirect to success page
+    else:
+        form = MemberRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'member_registration.html', context)
